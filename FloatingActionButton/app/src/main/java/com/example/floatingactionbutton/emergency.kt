5fbf1cc86925625dbc810badf82ed84bcbbdb80b
JaesungLeee <viki0114@naver.com>
2021-01-16 22:23:03 +0900

    [CRERATE] Expandable Floating Action Button

---
+++ FloatingActionButton/app/src/main/java/com/example/floatingactionbutton/emergency.kt
@@ -0,0 +1,11 @@
+package com.example.floatingactionbutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class emergency : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emergency)
+    }
+}
---
+++ FloatingActionButton/app/src/main/java/com/example/floatingactionbutton/logout.kt
@@ -0,0 +1,11 @@
+package com.example.floatingactionbutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class logout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logout)
+    }
+}
